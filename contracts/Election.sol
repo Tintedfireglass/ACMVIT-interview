99d86354d2fcdb96772f730bf766a420da360a00
Tanmay Paturu <tintedfireglass@hotmail.com>
2023-04-30 21:28:31 +0530

    Add files via upload

---
+++ contracts/Election.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.5.16;
+
+contract Election {
+
+    struct Candidate{
+        uint id;
+        string name;
+        uint votecount;
+    }
+
+    mapping(uint => Candidate)public candidates;
+    mapping(address => bool)public voters;
+
+    uint public candidatesCount;
+
+    constructor () public{
+        addCandidate("Candidate A");
+        addCandidate("Candidate B");
+        }
+
+    function addCandidate (string memory _name) private{
+        candidatesCount++;
+        candidates[candidatesCount]=Candidate(candidatesCount, _name,0);
+    }
+    function vote(uint  _candidateID) public{
+
+        require(!voters[msg.sender]);
+        require(_candidateID>0 && _candidateID <= candidatesCount);
+
+        voters[msg.sender] = true;
+
+        candidates[_candidateID].votecount++;
+        
+    }
+
+}  
